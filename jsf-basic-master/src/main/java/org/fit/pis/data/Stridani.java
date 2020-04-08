4f9cfabc8b5189a92cc85ea7f314d77c8ca95d83
iampeacock <m.kalabza@gmail.com>
2020-04-08 15:33:51 +0200

    Oprava - uppercase sql volani

--- jsf-basic-master/src/main/java/org/fit/pis/data/Stridani.java
+++ jsf-basic-master/src/main/java/org/fit/pis/data/Stridani.java
@@ -10,6 +10,7 @@ import java.sql.Timestamp;
  * 
  */
 @Entity
+@Table(name="Stridani")
 @NamedQuery(name="Stridani.findAll", query="SELECT s FROM Stridani s")
 public class Stridani implements Serializable {
 	private static final long serialVersionUID = 1L;
