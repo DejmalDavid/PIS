9d5a278f07dfed402db2cfd79ba9c38382274249
fero02 <56720675+fero02@users.noreply.github.com>
2020-04-22 11:06:15 +0200

    add position and sort tables

--- jsf-basic-master/client/src/app/group-details/group-details.component.ts
+++ jsf-basic-master/client/src/app/group-details/group-details.component.ts
@@ -14,7 +14,7 @@ import { flagsIcon } from '../img';
     styleUrls: ['./group-details.component.css']
 })
 
-export class GroupDetailsComponent implements OnInit { 
+export class GroupDetailsComponent implements OnInit {
 
     displayedColumns: string[] = ['position', 'flag', 'name', 'zapasy', 'skore', 'body'];
     dataSource = new MatTableDataSource();
@@ -26,12 +26,18 @@ export class GroupDetailsComponent implements OnInit {
     constructor(private route: ActivatedRoute,
         private api: ApiService) { }
     ngOnInit() {
-        
+
         this.route.paramMap.subscribe(params => {
             switch (params.get('source')) {
                 case "A":
                     this.api.getGroup("1").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[1];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[1]);
                     })
                     this.api.getGroupMatches("1").subscribe(data => {
@@ -42,12 +48,28 @@ export class GroupDetailsComponent implements OnInit {
                 case "B":
                     this.api.getGroup("2").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[2];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[2]);
+                    })
+                    this.api.getGroupMatches("2").subscribe(data => {
+                        var team = JSON.parse(JSON.stringify(data));
+                        this.DSmatches = new MatTableDataSource(team);
                     })
                     break;
                 case "C":
                     this.api.getGroup("3").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[3];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[3]);
                     })
                     this.api.getGroupMatches("3").subscribe(data => {
@@ -58,32 +80,82 @@ export class GroupDetailsComponent implements OnInit {
                 case "D":
                     this.api.getGroup("4").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[4];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[4]);
+                    })
+                    this.api.getGroupMatches("4").subscribe(data => {
+                        var team = JSON.parse(JSON.stringify(data));
+                        this.DSmatches = new MatTableDataSource(team);
                     })
                     break;
                 case "E":
                     this.api.getGroup("5").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[5];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[5]);
+                    })
+                    this.api.getGroupMatches("5").subscribe(data => {
+                        var team = JSON.parse(JSON.stringify(data));
+                        this.DSmatches = new MatTableDataSource(team);
                     })
                     break;
                 case "F":
                     this.api.getGroup("6").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[6];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[6]);
+                    })
+                    this.api.getGroupMatches("6").subscribe(data => {
+                        var team = JSON.parse(JSON.stringify(data));
+                        this.DSmatches = new MatTableDataSource(team);
                     })
                     break;
                 case "G":
                     this.api.getGroup("7").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[7];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[7]);
+                    })
+                    this.api.getGroupMatches("7").subscribe(data => {
+                        var team = JSON.parse(JSON.stringify(data));
+                        this.DSmatches = new MatTableDataSource(team);
                     })
                     break;
                 default:
                     this.dataSource = new MatTableDataSource(GROUPH);
                     this.api.getGroup("8").subscribe(data => {
                         var team = (data[0]);
+                        var t = team[8];
+                        console.log(t);
+                        t.sort(this.dynamicSort("body"));
+                        t.forEach((element, index) => {
+                            element.position = index + 1;
+                        });
                         this.dataSource = new MatTableDataSource(team[8]);
+                    })
+                    this.api.getGroupMatches("8").subscribe(data => {
+                        var team = JSON.parse(JSON.stringify(data));
+                        this.DSmatches = new MatTableDataSource(team);
                     })
                     break;
             }
@@ -96,9 +168,23 @@ export class GroupDetailsComponent implements OnInit {
     onHover(id) {
         this.matchID = id;
     }
-    setIcon(id){
+    setIcon(id) {
         alert(id);
         return flagsIcon[id]
+    }
+    dynamicSort(property) {
+        var sortOrder = 1;
+        if (property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+        return function (a, b) {
+            /* next line works with strings and numbers, 
+             * and you may want to customize it to your needs
+             */
+            var result = (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
+            return result * sortOrder;
+        }
     }
 
 }
--- jsf-basic-master/client/src/app/table-group/table-group.component.ts
+++ jsf-basic-master/client/src/app/table-group/table-group.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { GROUPA, GROUPB, GROUPC, GROUPD, GROUPE, GROUPF, GROUPG, GROUPH} from '../teams';
+import { GROUPA, GROUPB, GROUPC, GROUPD, GROUPE, GROUPF, GROUPG, GROUPH } from '../teams';
 import { ApiService } from '../api.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,61 +16,115 @@ export interface PeriodicElement {
 
 @Component({
   selector: 'app-table-group',
-  templateUrl: './table-group.component.html', 
+  templateUrl: './table-group.component.html',
   styleUrls: ['./table-group.component.css']
 })
 export class TableGroupComponent implements OnInit {
 
   displayedColumns: string[] = ['position', 'name', 'zapasy', 'body'];
   dataSource = new MatTableDataSource();
-  
-  constructor(private route: ActivatedRoute, private api: ApiService) {  
+
+  constructor(private route: ActivatedRoute, private api: ApiService) {
   }
   nation: string;
-  @Input() source :string;
+  @Input() source: string;
   ngOnInit() {
-    this.api.getAllTeams().subscribe(teams => { 
-     
-      
+    this.api.getAllTeams().subscribe(teams => {
+
+
       switch (this.source) {
         case "A":
           var team1 = JSON.parse(JSON.stringify(teams[0]));
+          var t = team1[1];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
           this.dataSource = new MatTableDataSource(team1[1]);
           break;
         case "B":
           var team1 = JSON.parse(JSON.stringify(teams[1]));
+          var t = team1[2];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
           this.dataSource = new MatTableDataSource(team1[2]);
           break;
         case "C":
           var team1 = JSON.parse(JSON.stringify(teams[2]));
+          var t = team1[3];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
           this.dataSource = new MatTableDataSource(team1[3]);
           break;
         case "D":
           var team1 = JSON.parse(JSON.stringify(teams[3]));
+          var t = team1[4];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
           this.dataSource = new MatTableDataSource(team1[4]);
           break;
         case "E":
           var team1 = JSON.parse(JSON.stringify(teams[4]));
+          var t = team1[5];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
           this.dataSource = new MatTableDataSource(team1[5]);
           break;
         case "F":
           var team1 = JSON.parse(JSON.stringify(teams[5]));
+          var t = team1[6];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
           this.dataSource = new MatTableDataSource(team1[6]);
           break;
         case "G":
           var team1 = JSON.parse(JSON.stringify(teams[6]));
+          var t = team1[7];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
           this.dataSource = new MatTableDataSource(team1[7]);
           break;
-      
+
         default:
-          var team1 = JSON.parse(JSON.stringify(teams[0]));
-          this.dataSource = new MatTableDataSource(team1[1]);
+          var team1 = JSON.parse(JSON.stringify(teams[7]));
+          var t = team1[8];
+          t.sort(this.dynamicSort("body"));
+          t.forEach((element, index) => {
+            element.position = index + 1;
+          });
+          this.dataSource = new MatTableDataSource(team1[8]);
           break;
       }
-  });
-    
+    });
+
   }
   onClick(home) {
     this.nation = home;
-}
+  }
+  dynamicSort(property) {
+    var sortOrder = 1;
+    if (property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a, b) {
+      /* next line works with strings and numbers, 
+       * and you may want to customize it to your needs
+       */
+      var result = (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
+      return result * sortOrder;
+    }
+  }
 }
